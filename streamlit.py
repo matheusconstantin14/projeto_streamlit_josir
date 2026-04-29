7764b1f4814ac4892c5f4f22a51ead61007a0d7d
Matheus Constantin Rodrigues de Souza <matheusconstantin14@gmail.com>
2026-04-29 17:46:19 +0000

    adfd

---
+++ streamlit.py
@@ -0,0 +1,5 @@
+import streamlit
+
+st.title('GTA 6 SERÁ LANÇADO AINDA EM 2026!')
+st.write('A Rockstar Games acaba de anunciar que o GTA 6 será lançado ainda em 2026, trazendo apenas porte para consoles como PlayStation 5 e XBox One Series S e Series X')
+st.image('https://upload.wikimedia.org/wikipedia/pt/4/46/Grand_Theft_Auto_VI.png')
